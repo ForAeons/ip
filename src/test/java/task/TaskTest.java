adfdbb6d9297d764f2180c93578ab1f40e64b0c6
ForAeons <w.xy020203@gmail.com>
2024-01-27 15:08:47 +0800

    fix: update test file to include taskid

--- src/test/java/task/TaskTest.java
+++ src/test/java/task/TaskTest.java
@@ -6,21 +6,21 @@ import org.junit.jupiter.api.Test;
 public class TaskTest {
   @Test
   public void testTaskToString() {
-    Task task = new Task("test task");
+    Task task = new Task(1, "test task");
     assertEquals("[ ] test task",task.toString());
 
-    Task task2 = new Task("test task", true);
+    Task task2 = new Task(1,"test task", true);
     assertEquals("[X] test task", task2.toString());
   }
 
   @Test
   public void testTaskGetDetails() {
-    Task task = new Task("test task");
+    Task task = new Task(1,"test task");
     assertEquals("test task", task.getDetails());
   }
 
   @Test void testTaskToggleDone() {
-    Task task = new Task("test task");
+    Task task = new Task(1,"test task");
     task.toggleDone();
     assertEquals("[X] test task", task.toString());
     task.toggleDone();
@@ -29,28 +29,28 @@ public class TaskTest {
 
   @Test
   public void testTodoToString() {
-    Todo todo = new Todo("test todo");
+    Todo todo = new Todo(1,"test todo");
     assertEquals("[T][ ] test todo", todo.toString());
 
-    Todo todo2 = new Todo("test todo", true);
+    Todo todo2 = new Todo(1,"test todo", true);
     assertEquals("[T][X] test todo", todo2.toString());
   }
 
   @Test
   public void testDeadlineToString() {
-    Deadline deadline = new Deadline("test deadline", "Aug 23 2020");
+    Deadline deadline = new Deadline(1,"test deadline", "Aug 23 2020");
     assertEquals("[D][ ] test deadline (by: Aug 23 2020)", deadline.toString());
 
-    Deadline deadline2 = new Deadline("test deadline", "Aug 23 2020", true);
+    Deadline deadline2 = new Deadline(1,"test deadline", "Aug 23 2020", true);
     assertEquals("[D][X] test deadline (by: Aug 23 2020)", deadline2.toString());
   }
 
   @Test
   public void testEventToString() {
-    Event event = new Event("test event", "Aug 23 2020", "Aug 24 2020");
+    Event event = new Event(1,"test event", "Aug 23 2020", "Aug 24 2020");
     assertEquals("[E][ ] test event (from: Aug 23 2020 to: Aug 24 2020)", event.toString());
 
-    Event event2 = new Event("test event", "Aug 23 2020", "Aug 24 2020", true);
+    Event event2 = new Event(1,"test event", "Aug 23 2020", "Aug 24 2020", true);
     assertEquals("[E][X] test event (from: Aug 23 2020 to: Aug 24 2020)", event2.toString());
   }
 }
